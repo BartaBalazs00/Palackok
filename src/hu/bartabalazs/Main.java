8d115fe503b2c6ea9631d2b1a668dc7438f1367b
Barta Balázs <barta.balazs00@gmail.com>
2021-10-06 16:30:40 +0200

    Teszt osztály rendes elkészítése

--- src/hu/bartabalazs/Main.java
+++ src/hu/bartabalazs/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         Teszt teszt = new Teszt();
-        teszt.elso();
     }
 }
--- src/hu/bartabalazs/Teszt.java
+++ src/hu/bartabalazs/Teszt.java
@@ -1,10 +1,55 @@
 package hu.bartabalazs;
 
-public class Teszt {
-    public Teszt(){
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Teszt {
+    private ArrayList<Rekesz> rekeszek;
+    public Teszt(){
+        rekeszek = new ArrayList<>();
+        //elso();
+        masodik();
     }
-    public void elso(){
+
+    private void masodik() {
+        beolvas();
+    }
+
+    private void beolvas() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("palackok.txt"));
+            rekeszek.add(new Rekesz(10000));
+            String sor = br.readLine();
+            while (sor != null){
+                Palack p;
+                String[] adatok = sor.split(";");
+                if (adatok.length == 2){
+                    p = new Palack(adatok[0], Integer.parseInt(adatok[1]));
+                } else if(adatok.length == 3){
+                    p = new Palack(adatok[0], Integer.parseInt(adatok[1]), Integer.parseInt(adatok[2]));
+                } else {
+                    p = new VisszaValthatoPalack(adatok[0], Integer.parseInt(adatok[1]), Integer.parseInt(adatok[2]), Integer.parseInt(adatok[3]));
+                }
+                boolean siker = rekeszek.get(rekeszek.size()-1).ujPalack(p);
+                if (!siker){
+                    rekeszek.add(new Rekesz(10000));
+                }
+                sor = br.readLine();
+                br.close();
+            }
+        }catch (FileNotFoundException e){
+            e.getMessage();
+        } catch (IOException e){
+            e.getMessage();
+        } catch (Exception e){
+            e.getMessage();
+        }
+    }
+
+    private void elso(){
         Palack p = new Palack("tej", 1000, 1000);
         Palack p2 = new Palack("Kakaó", 500, 200);
         VisszaValthatoPalack vp1 = new VisszaValthatoPalack("Kóla",200, 200, 25);
