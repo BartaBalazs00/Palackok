7bab2a3f43d3028a0e0516ba31ea0c7d15b615eb
Barta Balázs <barta.balazs00@gmail.com>
2021-10-06 15:43:03 +0200

    példák elkészítése

--- src/hu/bartabalazs/Main.java
+++ src/hu/bartabalazs/Main.java
@@ -3,6 +3,11 @@ package hu.bartabalazs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Palack p = new Palack("tej", 1000, 1000);
+        Palack p2 = new Palack("Kakaó", 500, 200);
+        VisszaValthatoPalack vp1 = new VisszaValthatoPalack("Kóla",200, 200, 25);
+        System.out.println(p);
+        System.out.println(p2);
+        System.out.println(vp1);
     }
 }
