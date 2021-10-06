e9c6e65fd6ee309342bb5033e3731fda356c4c8e
Barta Balázs <barta.balazs00@gmail.com>
2021-10-06 16:09:48 +0200

    Teszt osztály létrehozása

--- src/hu/bartabalazs/Main.java
+++ src/hu/bartabalazs/Main.java
@@ -3,17 +3,7 @@ package hu.bartabalazs;
 public class Main {
 
     public static void main(String[] args) {
-        Palack p = new Palack("tej", 1000, 1000);
-        Palack p2 = new Palack("Kakaó", 500, 200);
-        VisszaValthatoPalack vp1 = new VisszaValthatoPalack("Kóla",200, 200, 25);
-
-        Rekesz r = new Rekesz(90000);
-        r.ujPalack(p);
-        r.ujPalack(p2);
-        r.ujPalack(vp1);
-        System.out.println(p);
-        System.out.println(p2);
-        System.out.println(vp1);
-        System.out.println(r);
+        Teszt teszt = new Teszt();
+        teszt.elso();
     }
 }
